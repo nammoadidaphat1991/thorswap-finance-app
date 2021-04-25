c776fb6da9ed49adf6a738bb8b06d971bf8ca670
skyorion427 <skyorion427@gmail.com>
2021-04-24 21:35:02 -0400

    Update threshold amount comment

--- src/multichain-sdk/constants/amount.ts
+++ src/multichain-sdk/constants/amount.ts
@@ -2,9 +2,9 @@
 export const DEFAULT_SLIP_LIMIT = 5
 
 // threshold amount enforced to retain in the wallet for gas purpose
-export const RUNE_THRESHOLD_AMOUNT = 1
-export const ETH_THRESHOLD_AMOUNT = 0.05
-export const BNB_THRESHOLD_AMOUNT = 0.01
-export const BTC_THRESHOLD_AMOUNT = 0.001
+export const RUNE_THRESHOLD_AMOUNT = 1 // $10
+export const ETH_THRESHOLD_AMOUNT = 0.05 // $100
+export const BNB_THRESHOLD_AMOUNT = 0.01 // $50
+export const BTC_THRESHOLD_AMOUNT = 0.001 // $100
 export const LTC_THRESHOLD_AMOUNT = 0.001
 export const BCH_THRESHOLD_AMOUNT = 0.001
