71960f02d44185961194ffd2c38b66b57d72aaa4
thorswapdev <thorswap.finance@gmail.com>
2021-05-09 09:15:21 -0400

    fix: rune threshold amount to 0.02

--- src/multichain-sdk/constants/amount.ts
+++ src/multichain-sdk/constants/amount.ts
@@ -4,10 +4,10 @@ import BigNumber from 'bignumber.js'
 export const DEFAULT_SLIP_LIMIT = 5
 
 // threshold amount enforced to retain in the wallet for gas purpose
-export const RUNE_THRESHOLD_AMOUNT = 0.1 // $10
-export const ETH_THRESHOLD_AMOUNT = 0 // $100
-export const BNB_THRESHOLD_AMOUNT = 0 // $50
-export const BTC_THRESHOLD_AMOUNT = 0 // $100
+export const RUNE_THRESHOLD_AMOUNT = 0.02 // gas fee for rune transfer/deposit
+export const ETH_THRESHOLD_AMOUNT = 0
+export const BNB_THRESHOLD_AMOUNT = 0
+export const BTC_THRESHOLD_AMOUNT = 0
 export const LTC_THRESHOLD_AMOUNT = 0
 export const BCH_THRESHOLD_AMOUNT = 0
 
