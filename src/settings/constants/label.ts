5446853ad097f130ad4f759802d05e6b2bd32931
thorswapdev <thorswap.finance@gmail.com>
2021-05-13 13:13:30 -0400

    Fix: tooltip for poolshare estimated

--- src/settings/constants/label.ts
+++ src/settings/constants/label.ts
@@ -7,7 +7,7 @@ export const TX_FEE_TOOLTIP_LABEL =
 export const ESTIMATED_TIME_LABEL = 'Estimated time to process the transaction'
 
 export const ESTIMATED_POOL_SHARE_LABEL =
-  'Estimated time to process the transaction'
+  'Estimated percentage of your pool share after providing the liquidity'
 
 export const MIN_RECEIVED_LABEL =
   'Your transaction will revert if there is a large, unfavorable price movement before it is confirmed.'
