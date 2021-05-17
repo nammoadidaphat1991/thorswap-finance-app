cd2218395070619c446f2884e77a962042740e61
thorswapdev <thorswap.finance@gmail.com>
2021-05-17 05:10:45 -0400

    Add active status for navbar items

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useMemo, useRef } from 'react'
 
 import { Menu as MenuIcon, X as XIcon } from 'react-feather'
+import { useLocation } from 'react-router-dom'
 
 import { useOnClickOutside } from 'hooks/useOnClickOutside'
 
@@ -9,6 +10,8 @@ import { navMenuList } from './data'
 import * as Styled from './Navbar.style'
 
 export const Navbar = () => {
+  const { pathname } = useLocation()
+
   const [isOpen, setOpen] = useState(false)
   const navRef = useRef(null)
 
@@ -28,11 +31,11 @@ export const Navbar = () => {
 
   const renderMenu = useMemo(() => {
     return navMenuList.map(({ link, label }) => (
-      <NavLink to={link} key={link}>
+      <NavLink to={link} key={link} active={pathname === link}>
         {label}
       </NavLink>
     ))
-  }, [])
+  }, [pathname])
 
   return (
     <nav ref={navRef}>
